1d03a0a39b0fe1bfaddc3c4decc4cdcc38800d22
Gabriel Carrion <gabo_c_v@hotmail.com>
2020-03-06 23:31:13 -0600

    first commit

---
+++ public/eq.js
@@ -0,0 +1,100 @@
+
+// window.onbeforeunload = function(){
+//   return 'Are you sure you want to leave?';
+// };
+var mapsKey = config.gmapskey
+var geonamesKey= config.geonameskey
+window.addEventListener('load',function(){
+
+  var script = document.createElement('script');
+  script.type = 'text/javascript';
+  script.src = `https://maps.googleapis.com/maps/api/js?key=${mapsKey}&callback=initMap`;
+  document.body.appendChild(script);
+});
+
+
+
+var geocoder
+var map
+var north
+var south
+var east
+var west
+var username = geonamesKey
+
+var options ={
+  zoom: 10,
+  center: {lat: 25.6866, lng: -100.3161}
+}
+
+
+function initMap() {
+ 
+  // The map, centered at Uluru
+   map = new google.maps.Map(
+      document.getElementById('map'), {options});
+
+  //Geocoder   
+  geocoder = new google.maps.Geocoder();
+
+  document.getElementById('search').addEventListener('click', function() {
+    codeAddress(geocoder, map);
+
+  });
+  
+  }
+
+
+  newSearch();
+// New Search
+
+function newSearch(){
+ 
+  document.getElementById('search').addEventListener('click', function() {
+    codeAddress(geocoder, map);
+  });
+}
+
+// Deletes previous
+ function deleteMarkers() {
+        clearMarkers();
+        markers = [];
+  }
+
+
+  function codeAddress (){
+    var address = document.getElementById('location').value;
+    geocoder.geocode( { 'address': address}, function(results, status) {
+      if (status == 'OK') {
+        var r = results[0].geometry.location
+        map.setCenter(r);
+        // var marker = new google.maps.Marker({
+        //     map: map,
+        //     position: r
+        // });
+      } else {
+        alert('Geocode was not successful for the following reason: ' + status);
+      }
+    });
+  }
+
+
+function geonamepi (map){
+    url = `http://api.geonames.org/earthquakesJSON?north=${north}&south=${south}&east=${east}&west=${west}&username=${username}`
+    fetch(url)
+    .then(resultAPI=>resultAPI.json())
+    .then(result => {
+        console.log(result)
+        result.earthquakes.map(earthquake => {
+            var marker = new google.maps.Marker({
+                map: map,
+                position: {
+                    lat: earthquake.lat,
+                    lng: earthquake.lng
+                }
+              });
+             
+            
+        })
+    })
+}
